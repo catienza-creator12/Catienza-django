16e6fde8e08a365314ec48ed26b014d2e327336c
catienza-creator <catienza.jonathan13@gmail.com>
2024-10-12 20:44:18 +0800

    commit

---
+++ Project/app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class HomepageView(TemplateView):
+    template_name = 'app/home.html'
+
+class AboutpageView(TemplateView):
+        template_name = 'app/about.html'
+
+
